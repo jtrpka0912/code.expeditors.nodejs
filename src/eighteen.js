17488576ada8155b675b674071fd679bc8fdba4b
J. Trpka <jtrpka0912@gmail.com>
2022-09-14 13:09:49 -0400

    Sorted by last name.

--- src/eighteen.js
+++ src/eighteen.js
@@ -1,6 +1,11 @@
 const printOverEighteen = (people) => {
   const organizedPeople = people
     .filter((person) => parseInt(person.age) > 18)
+    .sort((personA, personB) => {
+      if(personA.lastname < personB.lastname) return -1;
+      if(personA.lastname > personB.lastname) return 1;
+      return 0;
+    });
 
   console.info(organizedPeople, organizedPeople.length);
 };
