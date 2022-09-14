c96e14fe063073a97dd2a363fc352cda61009fcf
J. Trpka <jtrpka0912@gmail.com>
2022-09-14 11:48:34 -0400

    Characteristics are split.

--- src/gather.js
+++ src/gather.js
@@ -3,7 +3,22 @@ const fs = require('fs');
 const gatherPeople = () => {
 
   try {
-    const contents = fs.readFileSync(process.cwd() + '/INPUT.txt');
+    const data = fs.readFileSync(process.cwd() + '/INPUT.txt', 'utf-8');
+
+    // Split the data by either \r or \n
+    const lines = data.split(/\r?\n/);
+
+    const people = lines.map((line) => {
+
+      const characteristics = line
+        .replace('"', '') // Replace the first instance of d-quotes
+        .replace('"\\n', '') // Replace the end line and d-quotes
+        .split('","'); // Split by ","
+
+      console.info(characteristics);
+
+      return null;
+    });
   } catch(err) {
     console.error(err);
   }
